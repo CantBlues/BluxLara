78a8f15e68f7b4de205a70334808fcb87fb0bde7
Lan <wyslanbin@gmail.com>
2022-07-22 02:27:11 +0800

    sort Tasks month data

--- app/Http/Controllers/DailyTask.php
+++ app/Http/Controllers/DailyTask.php
@@ -48,7 +48,7 @@ class DailyTask extends Controller
         $lastmonth = [Carbon::now()->subDays(60),Carbon::now()->subDays(30)];
         $data["this_month"] = TaskType::selectRaw("count(tasks.id) as times,task_types.name,task_types.weight")
             ->join("tasks","task_types.id","=","tasks.type_id")->whereBetween("tasks.date",$thismonth)
-            ->groupBy("tasks.type_id")->where("tasks.mark",true)->get();
+            ->groupBy("tasks.type_id")->where("tasks.mark",true)->orderBy("times","desc")->get();
         $data["last_month"] = TaskType::selectRaw("count(tasks.id) as times,task_types.name,task_types.weight")
             ->join("tasks","task_types.id","=","tasks.type_id")->whereBetween("tasks.date",$lastmonth)
             ->groupBy("tasks.type_id")->where("tasks.mark",true)->get();
